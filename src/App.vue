41301473b2b91b7dd0996bb5a781df96f02038a2
zhaoce <zhaoce@huivo.com>
2019-11-18 18:04:44 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
@@ -28,5 +28,111 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+@media screen and (max-height: 1602px) {
+  html {
+    font-size: 426.66px;
+  }
+}
+@media screen and (max-height: 1026px) {
+  html {
+    font-size: 273.06px;
+  }
+}
+@media screen and (max-height: 802px) {
+  html {
+    font-size: 213.32px;
+  }
+}
+@media screen and (max-height: 770px) {
+  html {
+    font-size: 204.8px;
+  }
+}
+@media screen and (max-height: 752px) {
+  html {
+    font-size: 200px;
+  }
+}
+@media screen and (max-height: 722px) {
+  html {
+    font-size: 192px;
+  }
+}
+@media screen and (max-height: 642px) {
+  html {
+    font-size: 170.66px;
+  }
+}
+@media screen and (max-height: 602px) {
+  html {
+    font-size: 160px;
+  }
+}
+@media screen and (max-height: 542px) {
+  html {
+    font-size: 144px;
+  }
+}
+@media screen and (max-height: 514px) {
+  html {
+    font-size: 136.52px;
+  }
+}
+@media screen and (max-height: 482px) {
+  html {
+    font-size: 128px;
+  }
+}
+@media screen and (max-height: 416px) {
+  html {
+    font-size: 110.4px;
+  }
+}
+@media screen and (max-height: 414px) {
+  html {
+    font-size: 109.86px;
+  }
+}
+@media screen and (max-height: 402px) {
+  html {
+    font-size: 106.66px;
+  }
+}
+@media screen and (max-height: 386px) {
+  html {
+    font-size: 102.4px;
+  }
+}
+@media screen and (max-height: 377px) {
+  html {
+    font-size: 100px;
+  }
+}
+@media screen and (max-height: 362px) {
+  html {
+    font-size: 96px;
+  }
+}
+@media screen and (max-height: 322px) {
+  html {
+    font-size: 85.32px;
+  }
+}
+@media screen and (max-height: 302px) {
+  html {
+    font-size: 80px;
+  }
+}
+@media screen and (max-height: 272px) {
+  html {
+    font-size: 72px;
+  }
+}
+@media screen and (max-height: 242px) {
+  html {
+    font-size: 64px;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,179 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video-player  class="video-play-box"
+        ref="videoPlayer"
+        :options="playerOptions"
+        :playsinline="true"
+        customEventName="customstatechangedeventname"
+
+        @ready="playerReadied">
+    </video-player>
+    <!-- 自定义控件 -->
+    <div class="video-play-custom">
+      <div class="video-play-mask" @click="togglePopup">
+        <van-icon class="video-play-button" v-if="playIconFlag" :name="playIcon" size="0.5rem" @click.stop="videoPlay"/>
+      </div>
+      <!-- 底部 -->
+      <van-popup class="video-play-col" v-model="show" position="bottom" :style="{ height: '1.28rem' }" :overlay="false">
+        <div class="video-col-list">
+
+        </div>
+        <div class="video-res-list">
+          <swiper :options="swiperOption" ref="mySwiper">
+            <swiper-slide class="swipe-item" v-for="item in resList" :key="item.id">
+              <p class="video-res-item-con">{{item.name}}</p>
+              <div class="video-res-item-mask"></div>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </van-popup>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { Popup } from 'vant';
+import { Button } from 'vant';
+import { Icon } from 'vant';
+import 'video.js/dist/video-js.css';
+import { videoPlayer } from 'vue-video-player';
+import 'swiper/dist/css/swiper.css';
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 export default {
   name: 'home',
+  data(){
+    return {
+      show: false,
+      playIcon: require('../assets/collection-icon-play.png'),
+      playIconFlag: true,
+      resList: [
+        {id: '111',name: '第一页: 教育介绍', src: require('../assets/demo1.mp4')},
+        {id: '222',name: '第二页: 数学(一)', src: require('../assets/demo2.mp4')},
+        {id: '333',name: '第三页: 数学(二)', src: require('../assets/demo3.mp4')},
+        {id: '444',name: '第四页: 科学', src: require('../assets/demo4.mp4')},
+        {id: '555',name: '第五页: 艺术', src: require('../assets/demo5.mp4')},
+        {id: '666',name: '第六页: 语言(一)', src: require('../assets/demo6.mp4')},
+        {id: '777',name: '第七页: 语言(二)', src: require('../assets/demo7.mp4')},
+        {id: '888',name: '第八页: 舞蹈', src: require('../assets/demo8.mp4')}
+      ],
+      playerOptions: {
+          // videojs options
+        height: document.documentElement.clientHeight,
+        width: document.documentElement.clientWidth,
+
+        // playbackRates: [1.0, 1.5, 2.0],
+        sources: [{
+          type: "video/mp4",
+          src: require('../assets/demo1.mp4')
+        }],
+        loop: false,
+        preload: true,
+        // poster: require('../assets/logo.png'),
+        controls: false
+      },
+      swiperOption: {
+          // some swiper options/callbacks
+          // 所有的参数同 swiper 官方 api 参数
+          // ...
+        }
+    }
+  },
   components: {
-    HelloWorld
+    [Popup.name]: Popup,
+    [Button.name]: Button,
+    [Icon.name]: Icon,
+    videoPlayer,
+    swiper,
+    swiperSlide
+  },
+  computed: {
+    player() {
+      return this.$refs.videoPlayer.player
+    },
+    swiper() {
+      return this.$refs.mySwiper.swiper
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    togglePopup (){
+      if(this.show == false){
+        if(!this.playIconFlag){
+          window.console.log('show');
+          this.show = true;
+        }
+      }else{
+        window.console.log('hide');
+        this.show = false;
+      }
+    },
+    playerReadied() {
+      // console.log(111);
+      // console.log('the player is readied', player)
+    },
+    videoPlay() {
+      this.player.play();
+      this.playIconFlag = false;
+    }
   }
 }
 </script>
+<style lang="less" scoped>
+.video-play-custom{
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  // 播放按钮
+  .video-play-mask{
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  
+    .video-play-button{
+      background: #fff;
+      border-radius: 0.5rem;
+      position: absolute;
+      left: 50%;
+      top: 45%;
+      transform: translate(-50%,-50%);
+    }
+  }
+  // 播放控制器
+  .video-play-col{
+    background-color: rgba(0,0,0,0.6);
+
+    .video-col-list{
+      height: 0.6rem;
+      background: rgba(255,255,255,0.6);
+    }
+    .video-res-list{
+
+      .swipe-item{
+        width: 1rem;
+        height: 0.56rem;
+        background: rgba(255,255,255,1);
+        border-radius: 4px;
+        margin: 0 6px;
+
+        .video-res-item-con{
+          padding: 6px;
+          font-size: 0.12rem;
+          color: #000;
+        }
+      }
+    }
+
+  }
+}
+
+
+</style>
